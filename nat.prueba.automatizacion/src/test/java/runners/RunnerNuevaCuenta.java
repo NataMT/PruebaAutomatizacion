1582e75eb605b30ffdb9b412371c0bd3fb0a1d66
NataMT <naty.morat@gmail.com>
2021-08-01 12:47:24 -0400

    Fin Busqueda

--- nat.prueba.automatizacion/src/test/java/runners/RunnerNuevaCuenta.java
+++ nat.prueba.automatizacion/src/test/java/runners/RunnerNuevaCuenta.java
@@ -6,9 +6,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(value = Cucumber.class)
-@CucumberOptions(features = "features/NuevaCuenta.feature", 
-glue = { "stepDefinitions" }, 
-tags = "@tagCrear1")
+@CucumberOptions(features = "features/NuevaCuenta.feature", glue = {
+		"stepDefinitions" }, tags = "@tagCrear1", plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
+				"json:target/cucumber-reports/CucumberTestReport.json",
+				"rerun:target/cucumber-reports/rerun.txt" }, monochrome = true)
 public class RunnerNuevaCuenta {
 
 }
--- nat.prueba.automatizacion/src/test/java/stepDefinitions/StepsBusqueda.java
+++ nat.prueba.automatizacion/src/test/java/stepDefinitions/StepsBusqueda.java
@@ -14,82 +14,74 @@ import io.cucumber.java.en.When;
 
 //Ejecución del paso a paso
 
-public class StepsBusqueda{
-	
+public class StepsBusqueda {
+
 	WebDriver driver = null;
-		
+
 	@Given("entre a la pagina")
 	public void entrar_al_sitio() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nats\\git\\pruebaAutomatizacion\\nat.prueba.automatizacion\\driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Nats\\git\\pruebaAutomatizacion\\nat.prueba.automatizacion\\driver\\chromedriver.exe");
 		driver = new ChromeDriver();
-		
+
 		driver.get("https://advantageonlineshopping.com/#/");
-		
+
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+
 	}
 
-
-	
 	@And("encuentro el buscador")
 	public void encuentro_el_buscador() {
-		
-		//Click en el icono search
+
+		// Click en el icono search
 		driver.findElement(By.id("menuSearch")).click();
-		
-	 
+
 	}
 
 	@When("escribo mouse y comienzo la busqueda")
-	public void comienzo_la_busqueda() throws InterruptedException {
-		
-		//Se ingresa la busqueda
+	public void comienzo_la_busqueda() {
+
+		// Se ingresa la busqueda
 		driver.findElement(By.id("autoComplete")).sendKeys("mouse");
-		
-		Thread.sleep(2000);
-	  
+
 	}
 
-	
 	@And("se despliegan los resultados")
-	public void ver_todos() {
-		
-		
-		//Click en "View All"
-		//class searchPopUp
+	public void ver_todos() throws InterruptedException {
+
+		// Click en "View All"
+		// class searchPopUp
 		driver.getPageSource().contains("searchPopUp");
 		driver.findElement(By.linkText("View All")).click();
-	    
-		//cerrar buscador
+
+		// cerrar buscador
+
+		driver.findElement(By.xpath("//img[contains(@src,'/css/images/closeDark.png')]")).click();
 		
-		driver.findElement(By.cssSelector("//img[contains(@src,'/css/images/closeDark.png')]")).click();
-		
+		Thread.sleep(2000);
 	}
-	
 
 	@Then("navego por los resultados")
-	public void navego_por_la_pagina() {
-		
-		//Navegar resultados
-		
+	public void navego_por_la_pagina() throws InterruptedException {
+
+		// Navegar resultados
+
 		driver.getPageSource().contains("Search result: ");
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("");
+		js.executeScript("window.scrollBy(0, 1204)");
 		
-		
-	    
-	}
+		Thread.sleep(5000);
 
+
+	}
 
 	@And("cierro el navegador")
 	public void cerrar_navegador() {
-		
-		//Cerrar el navegador 
+
+		// Cerrar el navegador
 		driver.close();
 	}
-	
-	
+
 }
-	
